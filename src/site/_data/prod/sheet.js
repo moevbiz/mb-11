d1b6b457971061de6212966a6769b984f22b8a75
Bruno <bruno.mokross@gmail.com>
2021-01-26 20:29:52 +0100

    add legend to color dots

--- src/site/_data/prod/sheet.js
+++ src/site/_data/prod/sheet.js
@@ -6,24 +6,39 @@ const seed   = require('../../../utils/save-seed.js');
 // via a URL of this form. We just need to pass in the ID of the sheet
 // which we can find in the URL of the document.
 const sheetID = "1XN9AyaFrvrVdw53PUFvCFSBElc0u35Eb4ZHnncdS91w";
-const googleSheetUrl = `https://spreadsheets.google.com/feeds/list/${sheetID}/od6/public/values?alt=json`;
+const sheet = {
+  sites: `https://spreadsheets.google.com/feeds/list/${sheetID}/1/public/values?alt=json`,
+  tech: `https://spreadsheets.google.com/feeds/list/${sheetID}/2/public/values?alt=json`,
+}
+
+const getSites = () => {
+  return axios.get(sheet.sites);
+}
+
+const getTech = () => {
+  return axios.get(sheet.tech);
+}
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
 
-    console.log(`Requesting data from ${googleSheetUrl}`);
-
-    axios.get(googleSheetUrl)
-      .then(response => {
+    Promise.all([getSites(), getTech()])
+      .then(([sites, tech]) => {
         // massage the data from the Google Sheets API into
         // a shape that will more convenient for us in our SSG.
-        console.log(response.data.feed);
         var data = {
           "content": [],
           "types": [],
+          "tech": [],
         };
-        var years = []
-        response.data.feed.entry.forEach(item => {
+        let years = [];
+        tech.data.feed.entry.forEach(item => {
+          data.tech.push({
+            "name": item.gsx$tech.$t,
+            "color": item.gsx$color.$t
+          });
+        })
+        sites.data.feed.entry.forEach(item => {
           function yearDisplay(item) {
             if (years.includes(item.gsx$year.$t)) {
               return false
